a12902ce51194d0c494f67546b1b58ba9ce83659
もふみつ <mitsuki.momoka@i.softbank.jp>
2025-11-08 15:05:46 +0900

    post_trend.py を更新

--- post_trend.py
+++ post_trend.py
@@ -14,7 +14,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 # ------------------------------
 # 🔐 環境変数
@@ -102,7 +101,7 @@ def get_trend_word():
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
 
-        driver = webdriver.Chrome(options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options) 
         driver.get("https://getdaytrends.com/japan/")
         time.sleep(7)
 
